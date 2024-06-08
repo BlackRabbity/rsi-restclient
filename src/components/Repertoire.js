4d8fe798ecd65580e98b2ca56bd02eab79bb55ef
Michal <michal332111@o2.pl>
2024-06-08 16:59:02 +0200

    Add display a list of showings with their images

--- src/components/Repertoire.js
+++ src/components/Repertoire.js
@@ -1,10 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/Repertoire.css';
+import { getShowingData } from '../services/cinemaService';
+import { getImageData } from '../services/imageService';
+import { formatDate } from '../utils/dateUtils';
 
-function Repertoire() {
+
+const Repertoire = () => {
+  const [showings, setShowings] = useState([]);
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const cinemaData = await getShowingData();
+        setShowings(cinemaData);
+
+        const imagePromises = cinemaData.map(async (showing) => {
+          const imageUrl = await getImageData({ ImageName: showing.film.imageName });
+          return { id: showing.id, imageUrl };
+        });
+
+        const imageResults = await Promise.all(imagePromises);
+        const imagesMap = imageResults.reduce((acc, curr) => {
+          acc[curr.id] = curr.imageUrl;
+          return acc;
+        }, {});
+        setImages(imagesMap);
+      } catch (error) {
+        console.error("Error fetching data", error);
+      }
+    };
+    fetchData();
+    return () => {
+    };
+  }, []);
+
+
   return (
-    <div className="repertoire">
-        <h1> TEST Repertoire </h1>
+    <div>
+      <h1>Showings</h1>
+      <ul>
+        {showings.map((showing, index) => (
+          <li key={index} className="showing-item">
+            <div>
+              {images[showing.id] && <img src={images[showing.id]} alt={showing.film.title} className="showing-image" />}
+            </div>
+            <div className="showing-details">
+              <h2>{showing.film.title}</h2>
+              <p>
+                <strong>Description:</strong> {showing.film.description}
+              </p>
+              <p>
+              <strong>Date of Play:</strong> {formatDate(showing.date)}
+              </p>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
--- src/services/cinemaService.js
+++ src/services/cinemaService.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 const API_URL = 'https://localhost:7043';
 
-export const getCinemaData = async () => {
+export const getShowingData = async () => {
     try {
-        const response = await axios.get(`${API_URL}/api/Cinema`);
+        const response = await axios.get(`${API_URL}/api/showings`);
         return response.data;
     } catch (error) {
         console.error('Error fetching data', error);
